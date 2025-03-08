93c14d80f2b06a6ed581c47f7e7ac1f12506b5b1
Youwei Zhen <youweizhen666@gmail.com>
2025-03-08 15:01:19 +0100

    test yw

---
+++ test.py
@@ -0,0 +1,11 @@
+#test youwei 
+class PositionEmbedding(torch.nn.Module):
+   def __init__(self):
+      super().init__()
+
+      # pos est le 
+      def get_pe(pos,i,d_model):
+         fenmu = 1e4**(i/d_model)
+         pe = pos/fenmu
+
+
